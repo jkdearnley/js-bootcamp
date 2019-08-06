77410bc69dba32af9db427084be869cbe3088908
jkdearnley <jkdearnley@gmail.com>
2019-08-07 10:52:22 +1200

    Test

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello World')
+// var name = 'Jayden'
+// console.log(name)
+
+// bla = 2
+// var bla
+// console.log(bla)
+
+let name = "Nelson"
+name = "NZ"
+name = "Jayden"
+console.log(name)
+
+const lastName = "Dearnley"
+const fistName = "Jayden"
+console.log(lastName)
+console.log(fistName)
+
+const numOne = 5
+const numTwo = 12
+const result = numTwo % numOne
+console.log(result)
